a4158bd594d7baf368898b2c0106ef0673f5db19
Gavin Travis <g.r.travis.2@gmail.com>
2025-10-05 09:07:54 -0700

    changed parser to a generator to prevent too much mem usage

--- big_data/scripts/load_data/load.py
+++ big_data/scripts/load_data/load.py
@@ -5,43 +5,36 @@ from __future__ import annotations
 import argparse
 import csv
 from pathlib import Path
+from typing import Generator
 
 from scripts.load_data.data_types import YouTubeData
 
 
-def _parse_file(file_path: Path) -> list[YouTubeData]:
-    """Given a data file path, open and parse it into a list of YT data."""
-    if file_path.is_file() is False:
-        msg = f"file_path does not point to a file: {file_path}"
-        raise FileNotFoundError(msg)
-
-    with file_path.open(encoding="utf-8") as f:
+def _parse_file(path: Path) -> list[YouTubeData]:
+    with path.open(encoding="utf-8") as f:
         reader = csv.reader(f, delimiter="\t")
 
-        data = []
-        for line in reader:
+        data: list[YouTubeData] = []
+        for num, line in enumerate(reader):
             try:
                 data.append(YouTubeData.from_csv(line))
             except TypeError:
-                print(line)
+                print(f"failed to parse {num}: {line}")
 
         return data
 
 
-def _parse_directory(dir_path: Path) -> dict[str, list[YouTubeData]]:
+def _iter_data(dir_path: Path) -> Generator[tuple[str, list[YouTubeData]]]:
     """Parse each file in directory returning {'directory name', data}."""
     if dir_path.is_dir() is False:
         msg = f"dir_path does not point to a directory: {dir_path}"
         raise FileNotFoundError(msg)
 
-    data = {}
     for path in dir_path.iterdir():
         if path.is_dir():
-            data |= _parse_directory(path)  # merge keys, values from below
+            yield from _iter_data(path)  # merge keys, values from below
         elif path.is_file() and not path.stem.startswith("log"):
-            data[dir_path.stem] = _parse_file(path)  # add this file key, value
-
-    return data
+            yield path.parent.stem, _parse_file(path)
 
 
 def main() -> None:
@@ -64,4 +57,5 @@ def main() -> None:
         msg = f"dir-path is not a file: {args.dir_path}"
         raise argparse.ArgumentError(None, msg)
 
-    # result = _parse_directory(args.dir_path)
+    for dir_name, data in _iter_data(args.dir_path):
+        print(f"{len(data)} lines read from file in folder {dir_name}\n")

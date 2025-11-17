2509c491febb8ad1cfc47789973c49b44efd9bff
travoloney <travismbolster@gmail.com>
2025-11-16 20:50:53 -0800

    Correlation analysis script added. loads mongodb youtube dataset to a spark data frame and then creates a sub data frame of numeric fields and run correlations every pair of numeric fields and returns the results of positive negative and near zero correlations

---
+++ test_jars.py
@@ -0,0 +1,15 @@
+﻿from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder
+        .appName("JarTest")
+        .config("spark.jars",
+                "file:///C:/hadoop/lib/mongo-spark-connector_2.12-3.0.2.jar,"
+                "file:///C:/hadoop/lib/mongodb-driver-sync-4.3.4.jar,"
+                "file:///C:/hadoop/lib/mongodb-driver-core-4.3.4.jar,"
+                "file:///C:/hadoop/lib/bson-4.3.4.jar")
+        .getOrCreate()
+)
+
+print("Spark loaded JARs:", spark.sparkContext._conf.get("spark.jars"))
+spark.stop()

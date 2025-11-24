79a797ec8b77a45b18192d27e1ca5b92c3761cdf
Gavin Travis <g.r.travis.2@gmail.com>
2025-11-24 15:20:52 -0800

    analyze_links script good to go, need gf still

--- big_data/scripts/analysis/_cluster.py
+++ big_data/scripts/analysis/_cluster.py
@@ -4,8 +4,9 @@ import pathlib
 import subprocess
 
 
-def spark_submit(path: pathlib.Path, packages: str) -> None:
-    """Pass the given path to spark cluster to queue the job."""
+def spark_submit(script_name: str, packages: str) -> None:
+    """Pass the given script **inside analysis folder** to spark cluster to queue the job."""
+    path = pathlib.Path("big_data/scripts/analysis/") / script_name
     subprocess.run(
         [
             "docker",

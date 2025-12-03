7be538b08e78542135b71b058c091b3b0baa55e6
Dylan Kopitzke <dbkopitzke@gmail.com>
2025-12-03 22:58:28 +0000

    fix mongo connection

--- big_data/scripts/analysis/trending_predictor.py
+++ big_data/scripts/analysis/trending_predictor.py
@@ -15,12 +15,12 @@ def new_spark_session(app_name: str, *, host: str = "localhost", db_host: str = 
     """Create connection to mongodb using sparkSession object."""
     mongo_read_uri = f"mongodb://{db_host}/youtube_analysis.videos"
     mongo_write_uri = f"mongodb://{db_host}/youtube_analysis.trendCollection"
-    #mongo_conn = "org.mongodb.spark:mongo-spark-connector_2.12:10.5.0"
+    mongo_conn = "org.mongodb.spark:mongo-spark-connector_2.12:10.5.0"
     spark: SparkSession = (  # init connection stuff
         SparkSession.builder.config("spark.driver.host", host)  # type: ignore
         .config("spark.mongodb.read.connection.uri", mongo_read_uri)
         .config("spark.mongodb.write.connection.uri", mongo_write_uri)
-        #.config("spark.jars.packages", mongo_conn)
+        .config("spark.jars.packages", mongo_conn)
         .master("local")
         .appName(app_name)
         .getOrCreate()
